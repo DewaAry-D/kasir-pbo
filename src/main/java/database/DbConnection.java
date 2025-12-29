967b7a68638e8b0238a63dbc8b56e8036114513d
DewaAry-D <arysaputraadewamade@gmail.com>
2025-12-29 19:27:01 +0800

    Merge origin/dev/yogi into dev/all
    # Conflicts:
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst

--- src/main/java/database/DbConnection.java
+++ src/main/java/database/DbConnection.java
@@ -13,7 +13,7 @@ public class DbConnection {
 
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/kasir_pbo";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "A1r1y05<>";
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
